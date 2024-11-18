efd32056bda52d01074a98f35f72b53a26737cb9
CristianBrenesV <brenescris1@gmail.com>
2024-11-17 21:29:02 -0600

    Navbar axtualizada perfil, login y registro

--- TempleOfInk/Controllers/HomeController.cs
+++ TempleOfInk/Controllers/HomeController.cs
@@ -27,7 +27,10 @@ namespace TempleOfInk.Controllers
         {
             return View();
         }
-
+        public IActionResult RegistroUsuario()
+        {
+            return View();
+        }
         public IActionResult Galeria()
         {
             return View();
